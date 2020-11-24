421117a0905494b0f4f4ec9cddc7780f91c6e6c8
Korpo-spec <60465693+Korpo-spec@users.noreply.github.com>
2020-11-24 10:36:33 +0100

    baseCharacter class

---
+++ GameEngine/Character.cs
@@ -0,0 +1,18 @@
+using System;
+using Raylib_cs;
+
+namespace GameEngine
+{
+    public class Character : GameObject
+    {
+        private int hp;
+        Rectangle Body = new Rectangle(10, 10, 50, 50);
+        
+        public override Draw(){
+
+
+
+        }
+
+    }
+}
