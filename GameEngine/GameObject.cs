8fbf5a4ca2c62f7f3303ba978a2a178aa9953112
BandersGit <andy.i.flaskan@gmail.com>
2020-11-24 10:18:41 +0100

    List of game objects, positioning variables and update and draw class-methods

---
+++ GameEngine/GameObject.cs
@@ -0,0 +1,39 @@
+using System.Numerics;
+using System;
+using System.Numerics;
+using System.Collections.Generic;
+
+namespace GameEngine
+{
+    public class GameObject
+    {
+        private Vector2 position = new Vector2(0, 0);
+
+        private float rotation = 0f;
+
+        public static List<GameObject> gameObjects = new List<GameObject>();
+
+        public void Update()
+        {
+
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+        static void UpdateAll()
+        {
+
+        }
+
+        static void DrawAll()
+        {
+            
+        }
+    }
+
+    
+}
