b850f49d36283abf633b2b21d01124222f2071c1
BandersGit <andy.i.flaskan@gmail.com>
2020-11-24 10:53:54 +0100

    Created Animation class, and spritesheet handling

---
+++ GameEngine/Animation.cs
@@ -0,0 +1,26 @@
+using System;
+using Raylib_cs;
+
+namespace GameEngine
+{
+    public class Animation
+    {
+        private Image spritesheet;
+
+        private int frameSize;
+
+        private Image[] frameSequence;
+
+        private float time;
+
+        private void SplitImg()
+        {
+
+        }
+
+        public void GetCurrentFrame()
+        {
+            
+        }
+    }
+}
