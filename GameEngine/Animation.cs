57bd6a48df49c80da5e8617f34235214ed2beed1
Korpo-spec <60465693+Korpo-spec@users.noreply.github.com>
2020-12-15 10:58:47 +0100

    Animation draw function working

--- GameEngine/Animation.cs
+++ GameEngine/Animation.cs
@@ -6,13 +6,25 @@ namespace GameEngine
 {
     public class Animation
     { 
-        Texture2D spriteSheet = Raylib.LoadTexture(@"AnimTest.png");
-        Vector2 positionInTexture = new Vector2(0, 0);
-        Rectangle frameSize = new Rectangle(0, 0, 16, 12); 
+        
+        Texture2D spriteSheet;
+        public Vector2 positionInTexture = new Vector2(100, 200);
+        public Rectangle frameSize = new Rectangle(0, 0, 16, 12); 
 
-        public static void DrawAnimation(Texture2D spriteSheet, Rectangle frameSize, Vector2 positionInTexture)
+        public int scale= 4;
+
+        public Animation(){
+            Image sourceImg = Raylib.LoadImage(@"AnimTest.png");
+
+            Raylib.ImageResizeNN(ref sourceImg, sourceImg.width*scale, sourceImg.height*scale);
+            frameSize.height = frameSize.height*scale;
+            frameSize.width = frameSize.width*scale;
+
+            spriteSheet = Raylib.LoadTextureFromImage(sourceImg);
+        }
+        public void DrawAnimation(/*Texture2D spriteSheet, Rectangle frameSize, Vector2 positionInTexture*/)
         {
-            Raylib.DrawTextureRec(spriteSheet, frameSize, positionInTexture, Color.BLANK);
+            Raylib.DrawTextureRec(spriteSheet, frameSize, positionInTexture, Color.WHITE);
         }
     }
 }
